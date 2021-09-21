94b430bc80ae94c94cc2c25d16efbf18a1c80993
idancali <dan@fluidtrends.ch>
2021-09-21 15:39:52 +0200

    added support for eos registration #1 #2

--- src/handlers/system.ts
+++ src/handlers/system.ts
@@ -1,7 +1,17 @@
 export const request = async ({ session, event, eventlog }: any) => {
     eventlog("[system request]:", event)
-    console.log(session.chain)
-    return { message: "ok" }
+
+    const { call, data } = event 
+    let result: any = { message: "skipped" }
+
+    switch (call) {
+        case "register":
+            eventlog(`registering ${data.username} ...`)
+            result = await session.server.chain.eos.system(session.server.chain, "caccount", { username: data.username, pub_key: data.publicKey, did: `did:carmel:${data.cid}` })
+            break
+    }
+
+    return result 
 }
 
 export const response = async ({ session, event, eventlog }: any) => {
