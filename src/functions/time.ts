55c72e63e55df6783513f724c6cad842b0166810
idancali <dan@fluidtrends.ch>
2021-09-28 10:01:58 +0200

    added carmel return payload support in time handler #23

--- src/functions/time.ts
+++ src/functions/time.ts
@@ -3,7 +3,7 @@ import moment from 'moment'
 export const request = async ({ log, session, channel, id, from, type, data }: any) => {
     log(`getting time ...`, data)
 
-    return { timestamp: `${Date.now()}`, formatted: `${moment().format(data.format)}` } 
+    return { timestamp: `${Date.now()}`, formatted: `${moment().format(data.format)}`, carmel: data.carmel || {} } 
 }
 
 export const response = async ({ log, session, channel, data }: any) => {
